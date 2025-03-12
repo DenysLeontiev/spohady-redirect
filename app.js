a679ec09cca8adb54d9a782fa332c041a0b4038c
Denys Leon <leonden0046@gmail.com>
2025-03-12 15:08:25 +0200

    Initial

---
+++ app.js
@@ -0,0 +1,8 @@
+let currentUrl = window.location.href;
+let redirectBaseUrl = "https://www.google.com.ua/?hl=uk";
+
+let newUrl = currentUrl.replace("http://127.0.0.1:5500", redirectBaseUrl);
+
+console.log(newUrl);
+
+console.log(newUrl);
