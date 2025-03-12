e4d7d6ce56acd274bea11269e885d3332767bd9e
Denys Leon <leonden0046@gmail.com>
2025-03-13 00:14:40 +0200

    _

--- app.js
+++ app.js
@@ -8,4 +8,4 @@ function replaceBeforeThirdSlash(url, newDomain) {
 
 const updatedUrl = replaceBeforeThirdSlash("https://www.spohady.com/", "https://spohady.azurewebsites.net");
 
-console.log(updatedUrl);
+window.location.href = updatedUrl;
