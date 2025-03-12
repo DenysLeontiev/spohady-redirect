eeb0b2e4648417cc9eebf5019ad16495b7f3f665
Denys Leon <leonden0046@gmail.com>
2025-03-13 00:01:31 +0200

    _

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-function redirectToNewDomain(url) {
-    const newBase = "https://spohady.azurewebsites.net"; // https://spohady.azurewebsites.net
-    try {
-        const parsedUrl = new URL(url);
-        const newUrl = newBase + parsedUrl.pathname + parsedUrl.search + parsedUrl.hash;
-        console.log(newUrl);
-        
-        //window.location.href = newUrl; // Redirect to the new URL
-    } catch (error) {
-        console.error("Invalid URL", error);
-    }
-}
+let currentUrl = window.location.href;
 
-let inputUrl = "https://www.spohady.com";
+let resultUrl = "https://spohady.azurewebsites.net";
 
-redirectToNewDomain(inputUrl);
+console.log(currentUrl);
