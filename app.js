c177d8d09db89ff11adc914fcc899238afee5070
Denys Leon <leonden0046@gmail.com>
2025-03-13 00:06:57 +0200

    fix

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ let currentUrl = window.location.href;
 
 let resultUrl = "https://spohady.azurewebsites.net";
 
-console.log(currentUrl);
+function replaceBeforeThirdSlash(url, newDomain) {
+    return url.replace(/^(?:[^\/]*\/){2}[^\/]*/, newDomain);
+}
+
+const updatedUrl = replaceBeforeThirdSlash("https://www.spohady.com/", "https://spohady.azurewebsites.net");
+
+window.href = updatedUrl;
